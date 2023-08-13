5ecf758dcdae025bcffcea8a9e267e1b19b6af45
Neck1 <141334946+Neck1ch@users.noreply.github.com>
2023-08-14 00:49:24 +0300

    Delete kubejs directory

--- kubejs/client_scripts/tools.js
+++
@@ -1,5 +0,0 @@
-onEvent("item.tooltip", (event) => {
-    event.add(/minecraft:(wooden|stone|iron|golden|diamond|netherite)_(sword|shovel|(pick)?axe|hoe)/, [
-        Text.red("Disabled"),
-    ]);
-});
